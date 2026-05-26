5af146bdb20fadbd401214c86cc29e06e4104a3f
Ana2026 <anamarquez3439@gmail.com>
2026-05-26 20:59:41 -0300

    Se creo la Clase Ana

--- src/main/java/com/example/proyecto_colaborativo/HelloController.java
+++ src/main/java/com/example/proyecto_colaborativo/HelloController.java
@@ -6,9 +6,10 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+//<3
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
+// HOLAAAAAAAAAAAAAA soy e
