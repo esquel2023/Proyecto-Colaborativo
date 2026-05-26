70c9aba558e86b1d45cf1800e10ab63898d64649
xluuleex <lucia.mllmn@gmail.com>
2026-05-26 20:10:47 -0300

    hola ?

--- src/main/java/com/example/proyecto_colaborativo/Launcher.java
+++ src/main/java/com/example/proyecto_colaborativo/Launcher.java
@@ -1,7 +1,7 @@
 package com.example.proyecto_colaborativo;
 
 import javafx.application.Application;
-
+//hola soy Lu
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
