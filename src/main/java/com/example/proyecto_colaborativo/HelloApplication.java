ff39cd020bf113e5855d808c0fb0dfd1a7db9d0f
xluuleex <lucia.mllmn@gmail.com>
2026-07-02 20:50:55 -0300

    cambie toda la interfaz menu principal

--- src/main/java/com/example/proyecto_colaborativo/HelloApplication.java
+++ src/main/java/com/example/proyecto_colaborativo/HelloApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("cliente.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menuprincipal.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        System.out.println(getClass().getResource("style.css"));
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setTitle("Productos");
         stage.setScene(scene);
